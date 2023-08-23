cdaf6e23e6c3d8f730cf2a5cea80bff5b7e75903
Troy Dorman <troydorman@gmail.com>
2023-08-23 00:04:35 -0600

    keep trying

--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -11,7 +11,7 @@ const SubmitButton = ({ definedStops }) => {
       const response = await axios.post(
         process.env.REACT_APP_ENVIRONMENT === "development"
           ? process.env.REACT_APP_LOCAL_URL
-          : "https://164.92.107.159/api/stops",
+          : "164.92.107.159/api/stops",
         definedStops,
         {
           responseType: "blob", // Request the response as binary data
