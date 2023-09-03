d6330871beec52199d844b324e0a87e3cfc12308
Troy Dorman <troydorman@gmail.com>
2023-09-02 23:35:27 -0600

    added spinner functionality

--- src/components/PasteBox.jsx
+++ src/components/PasteBox.jsx
@@ -1,14 +1,22 @@
 import React, { useState } from "react";
 import parseData from "./parseData.js";
 import axios from "axios";
+import { css } from "@emotion/react";
+import { BeatLoader } from "react-spinners";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const PasteBox = ({ value, onChange }) => {
   const [pdfData, setPdfData] = useState(null);
+  const [loading, setLoading] = useState(false);
 
   const handleChange = async (event) => {
     try {
-     
-      console.log("event.target.value", event.target.value);
+      setLoading(true);
 
       const response = await axios.post(
         process.env.REACT_APP_ENVIRONMENT === "development"
@@ -27,8 +35,12 @@ const PasteBox = ({ value, onChange }) => {
       const pdfUrl = URL.createObjectURL(pdfBlob);
 
       setPdfData(pdfUrl);
+      setLoading(false);
+      event.target.value = ""; // Clear the input box content
     } catch (error) {
       console.error("Error generating PDF:", error);
+      setLoading(false);
+      event.target.value = ""; // Clear the input box content on error
     }
   };
 
@@ -46,25 +58,32 @@ const PasteBox = ({ value, onChange }) => {
         onChange={handleChange}
         className="border rounded p-2 w-full"
       />
-      {pdfData && (
-        <div>
-          <p>PDF generated! Choose an option:</p>
-          <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
-            <a href={pdfData} target="_blank" rel="noopener noreferrer">
-              View PDF
-            </a>{" "}
-          </button>{" "}
-          <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
-            <a
-              href={pdfData}
-              download="export.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Download PDF
-            </a>
-          </button>
+      {loading ? (
+        <div className="mt-4 text-center">
+          <p className="text-bold text-lg">Loading...</p>
+          <BeatLoader css={override} size={50} color={"red"} loading={loading} />
         </div>
+      ) : (
+        pdfData && (
+          <div>
+            <p>PDF generated! Choose an option:</p>
+            <button className="bg-red-500 hover.bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
+              <a href={pdfData} target="_blank" rel="noopener noreferrer">
+                View PDF
+              </a>{" "}
+            </button>{" "}
+            <button className="bg-red-500 hover.bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
+              <a
+                href={pdfData}
+                download="export.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download PDF
+              </a>
+            </button>
+          </div>
+        )
       )}
     </>
   );
--- src/components/SubmitButton.jsx
+++ src/components/SubmitButton.jsx
@@ -1,13 +1,25 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { css } from "@emotion/react";
+import { BeatLoader } from "react-spinners";
+
+const override = css`
+  display: block;
+  margin: 0 auto;
+  border-color: red;
+`;
 
 const SubmitButton = ({ definedStops }) => {
   const [pdfData, setPdfData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [showButton, setShowButton] = useState(true);
 
   const handleSubmit = async () => {
     console.log("Submit button clicked");
 
     try {
+      setLoading(true);
+
       const response = await axios.post(
         process.env.REACT_APP_ENVIRONMENT === "development"
           ? "http://localhost:8122/api/stops"
@@ -25,38 +37,55 @@ const SubmitButton = ({ definedStops }) => {
       const pdfUrl = URL.createObjectURL(pdfBlob);
 
       setPdfData(pdfUrl);
+      setLoading(false); // Turn off the spinner after processing
+      setShowButton(false); // Hide the button after success
     } catch (error) {
       console.error("Error generating PDF:", error);
+      setLoading(false); // Turn off the spinner in case of an error
     }
   };
 
   return (
     <div>
-      <button
-        className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5"
-        onClick={handleSubmit}
-      >
-        Generate PDF
-      </button>
-      {pdfData && (
-        <div>
-          <p>PDF generated! Choose an option:</p>
-          <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
-            <a href={pdfData} target="_blank" rel="noopener noreferrer">
-              View PDF
-            </a>{" "}
-          </button>{" "}
-          <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
-            <a
-              href={pdfData}
-              download="export.pdf"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Download PDF
-            </a>
-          </button>
+      {showButton && ( // Conditional rendering based on showButton state
+        <button
+          className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5"
+          onClick={handleSubmit}
+          disabled={loading} // Disable the button while loading
+        >
+          {loading ? "Generating..." : "Generate PDF"}
+        </button>
+      )}
+      {loading ? (
+        <div className="mt-4 text-center">
+          <BeatLoader
+            css={override}
+            size={50}
+            color={"red"}
+            loading={loading}
+          />
         </div>
+      ) : (
+        pdfData && (
+          <div>
+            <p>PDF generated! Choose an option:</p>
+            <button className="bg-red-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
+              <a href={pdfData} target="_blank" rel="noopener noreferrer">
+                View PDF
+              </a>{" "}
+            </button>{" "}
+            <button className="bg-red-500 hover.bg-red-600 text-white font-semibold py-2 px-4 rounded-full shadow-md transition duration-300 ease-in-out mt-5">
+              <a
+                href={pdfData}
+                download="export.pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Download PDF
+              </a>
+            </button>
+          </div>
+        )
       )}
     </div>
   );
